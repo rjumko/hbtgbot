3d8e87afc79f5faeebde106fa2c693d81b2092e8
Fijnon <romann631@gmail.com>
2024-03-27 16:37:29 +0700

    first commit

--- copy_scheduler/__main__.py
+++ copy_scheduler/__main__.py
@@ -1,33 +1,56 @@
 import logging
 import os
+import asyncpg
+import asyncio
+from environs import Env
 from time import sleep
 from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from copy_scheduler.utils import copy_data_from_table
-from copy_scheduler.db import get_users_urls
+from copy_scheduler.dbconnect import Request
 
 
-
-def copy_data_from_google_table():
-    lst = get_users_urls()
+async def copy_data_from_google_table(db: Request):
+    lst = await db.get_users_urls()
 
     for l in lst:
-        copy_data_from_table(l[0])
-
-def jobs():
-    copy_data_from_google_table()
+        await copy_data_from_table(db, l[0])
 
 
-if __name__ == "__main__":
+async def jobs(db: Request):
+    await copy_data_from_google_table(db)
+
+
+async def db_pool(env: Env):
+    return await asyncpg.create_pool(
+        user=env("DB__USER"),
+        password=env("DB__PASSWORD"),
+        database=env("DB__NAME"),
+        host=env("DB__HOST"),
+        port=env("DB__PORT"),
+        command_timeout=60,
+    )
+
+
+async def main():
+    env = Env()
+    env.read_env()
+    pool_connect = await db_pool(env)
+    db = Request(pool_connect)
     logging.basicConfig(
         level=logging.DEBUG,
-        format='[%(asctime)s] #%(levelname)-8s %(filename)s:'
-           '%(lineno)d - %(name)s - %(message)s',
+        format="[%(asctime)s] #%(levelname)-8s %(filename)s:"
+        "%(lineno)d - %(name)s - %(message)s",
         datefmt="%d/%m/%Y %H:%M:%S %p %Z",
-           )
+    )
     logging.getLogger(__name__).setLevel(logging.DEBUG)
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(jobs, 'interval', seconds=300)
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(jobs, "interval", seconds=300, kwargs={"db": db})
     scheduler.start()
     logging.info(os.getcwd())
     while True:
-        sleep(1)
+        await asyncio.sleep(1)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- copy_scheduler/utils.py
+++ copy_scheduler/utils.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import pandas as pd
 import logging
+from copy_scheduler.dbconnect import Request
 from copy_scheduler.db import get_url_google, delete_all_by_userid, add_users
 
 logging.basicConfig(
@@ -11,12 +12,12 @@ logging.basicConfig(
 logging.getLogger(__name__).setLevel(logging.DEBUG)
 
 
-def get_sheet_id(url: str) -> str:
+async def get_sheet_id(url: str) -> str:
     return "Неверный URL" if len(url.split("/")) < 7 else url.split("/")[5]
 
 
-def get_table_from_google(url: str, user_id: str) -> list[tuple]:
-    sheet_id = get_sheet_id(url)
+async def get_table_from_google(url: str, user_id: int) -> list[tuple]:
+    sheet_id = await get_sheet_id(url)
     df = pd.read_csv(
         f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv",
         header=0,
@@ -24,19 +25,19 @@ def get_table_from_google(url: str, user_id: str) -> list[tuple]:
     lst = list(tuple(sub) for sub in df)
     logging.info(lst)
     return [
-        (l[0], l[1], datetime.strptime(l[2].strip(), "%d.%m.%Y"), l[3], user_id)
+        (str(l[0]), l[1], datetime.strptime(l[2].strip(), "%d.%m.%Y"), l[3], user_id)
         for l in lst
     ]
 
 
-def copy_data_from_table(usr_id: str):
-    url = get_url_google(usr_id)
+async def copy_data_from_table(db: Request, usr_id: int):
+    url = await db.get_url_google(usr_id)
     if url:
         logging.info(usr_id)
         logging.info(url)
-        delete_all_by_userid(usr_id)
-        add_users(
-            get_table_from_google(
+        await db.delete_all_by_userid(usr_id)
+        await db.add_clients(
+            await get_table_from_google(
                 url,
                 usr_id,
             )
