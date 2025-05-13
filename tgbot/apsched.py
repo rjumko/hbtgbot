f9a1e464be375a1e3b31caeab504f3e108c9154a
Fijnon <romann631@gmail.com>
2025-05-13 11:15:56 +0700

    delet settins

--- tgbot/apsched.py
+++ tgbot/apsched.py
@@ -1,6 +1,6 @@
 from aiogram import Bot
 import logging
-from tgbot.utils import prep_hb_text
+from tgbot.utils import prep_hb_text, copy_data_from_table
 from tgbot.db.dbconnect import Request
 from aiogram.exceptions import TelegramForbiddenError, TelegramBadRequest
 from apscheduler_di import ContextSchedulerDecorator
@@ -13,6 +13,7 @@ async def send_message_cron2(
     bot: Bot, user_id: int, request: Request, apscheduler: ContextSchedulerDecorator
 ):
     logger.info(type(user_id))
+    await copy_data_from_table(request, str(user_id))
     lst = await request.get_clients_birthday_today(user_id=user_id)
     logger.info(lst)
     try:
